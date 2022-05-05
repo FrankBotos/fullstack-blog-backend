576c314a92dd46629fc87e05bad89d1ab9ae1cb3
Frank Botos <frankbotos@hotmail.com>
2022-05-05 09:50:36 -0400

    initial commit
    graphql server with schemas and queries set up

---
+++ src/db.js
@@ -0,0 +1,11 @@
+export let users = [
+    { id: 1, name: "John Doe", email: "john@gmail.com", age: 22 },
+    { id: 2, name: "Jane Doe", email: "jane@gmail.com", age: 23 },
+    { id: 3, name: "Person Three", email: "person@gmail.com", age: 25 }
+  ];  
+
+  export let posts  = [
+    { id: 1, title: "Blog Post 1", content: "This is content for blog post 1", slug: "blog-post-1" },
+    { id: 2, title: "Blog Post 2", content: "This is content for blog post 2", slug: "blog-post-2" },
+    { id: 3, title: "Blog Post 3", content: "This is content for blog post 3", slug: "blog-post-3" }
+  ];  
---
+++ src/resolvers.js
@@ -0,0 +1,77 @@
+import { users, posts } from "./db";
+
+const resolvers = {
+
+  Query: {
+    user: (parent, { id }, context, info) => {
+      return users.find(user => user.id == id);
+    },
+    users: (parent, args, context, info) => {
+      return users;
+    },
+
+    post: (parent, { id }, context, info) => {
+      return posts.find(post => post.id == id);
+    },
+    posts: (parent, args, context, info) => {
+      return posts;
+    }
+  },
+
+  Mutation: {
+    createUser: (parent, { id, name, email, age }, context, info) => {
+      const newUser = { id, name, email, age };
+
+      users.push(newUser);
+
+      return newUser;
+    },
+    updateUser: (parent, { id, name, email, age }, context, info) => {
+      let newUser = users.find(user => user.id === id);
+
+      newUser.name = name;
+      newUser.email = email;
+      newUser.age = age;
+
+      return newUser;
+    },
+    deleteUser: (parent, { id }, context, info) => {
+      const userIndex = users.findIndex(user => user.id === id);
+
+      if (userIndex === -1) throw new Error("User not found.");
+
+      const deletedUsers = users.splice(userIndex, 1);
+
+      return deletedUsers[0];
+    },
+
+    createPost: (parent, { id, title, content, slug }, context, info) => {
+      const newPost = { id, title, content, slug };
+      posts.push(newPost);
+      return newPost;
+    },
+    updatePost: (parent, { id, title, content, slug }, context, info) => {
+      let newPost = posts.find(post => post.id === id);
+
+      newPost.title = title;
+      newPost.content = content;
+      newPost.slug = slug;
+
+      return newPost;
+    },
+    deletePost: (parent, { id }, context, info) => {
+      const postIndex = posts.findIndex(post => post.id === id);
+
+      if (postIndex === -1) throw new Error("Post not found.");
+
+      const deletedPosts = posts.splice(postIndex, 1);
+
+      return deletedPosts[0];
+    }
+
+
+  }
+
+};
+
+export default resolvers;
